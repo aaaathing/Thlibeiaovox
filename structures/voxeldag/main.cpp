56002d89545e1c5d44636bfff118689d8e24c2f3
aaaathing <aarontao950@gmail.com>
2024-10-17 19:33:43 -0700

    correct things

---
+++ structures/voxeldag/main.cpp
@@ -0,0 +1,12 @@
+#include "storage.cpp"
+typedef Cubiquity::Volume VoxelDAG;
+
+
+/*
+const Internals::NodeStore& nodeStore = Internals::getNodes(volume()).nodes();
+nodeStore.data()
+somename
+
+SubDAGArray subDAGs = findSubDAGs(nodeStore, getRootNodeIndex(volume()));
+someothername
+*/
